558b7e67e3b3bfc9164b5487d2a18327acd4adf4
Jakub Chramosta <jakub.chramosta@qest.cz>
2026-03-10 19:58:07 +0100

    fix: change webhook route method from GET to POST in gitHub.router.ts

--- src/router/gitHub.router.ts
+++ src/router/gitHub.router.ts
@@ -3,6 +3,6 @@ import helloController from "../controller/hello.controller.js";
 
 const gitHubRouter = Router();
 
-gitHubRouter.get("/webhook", helloController.gitHubPRCreated);
+gitHubRouter.post("/webhook", helloController.gitHubPRCreated);
 
 export default gitHubRouter;
