2b475c295b4c5e7ccbcb11b39d431dbbfcfb0065
Jakub Chramosta <jakub.chramosta@qest.cz>
2026-03-18 19:28:50 +0100

    build:manz thingsssss

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import helloRouter from "./router/hello.router.js";
 import gitHubRouter from "./router/gitHub.router.js";
+import "reflect-metadata";
 
 const app = express();
 const port = 3000;
@@ -10,7 +11,6 @@ app.use(express.json());
 app.use("/hello", helloRouter);
 
 app.use("/github", gitHubRouter);
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
